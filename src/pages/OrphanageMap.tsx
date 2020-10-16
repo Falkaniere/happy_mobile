884b8fc448a466ec81f37ffe4a8fa0b49fd86893
Jonatas Falkaniere <jonatasfalkaniere@MacBook-Pro-de-Jonatas.local>
2020-10-16 20:39:22 -0300

    finish layout

--- src/pages/OrphanageMap.tsx
+++ src/pages/OrphanageMap.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import MapView, { Marker, Callout,  PROVIDER_GOOGLE } from 'react-native-maps'
-import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import mapMarker from '../images/map-marker.png'
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export default function OrphanageMap() {
   const navigation = useNavigation();
 
   function handleNavigateToOrphanageDetails(){
     navigation.navigate('OrphanageDetails');
+  }
+
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition');
   }
 
   return (
@@ -44,9 +49,9 @@ export default function OrphanageMap() {
       </MapView>
       <View style={styles.footer} >
         <Text style={styles.footerText}>2 orfanatos encontrados</Text>
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+        <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
           <Feather name="plus" size={20} color="#FFF" />
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
   );
